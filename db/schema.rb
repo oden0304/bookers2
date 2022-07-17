51f1d9b22802898020ca06f571d548b60c93dc94
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-07-17 07:31:59 +0000

    [add]headerレイアウト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_16_073002) do
+ActiveRecord::Schema.define(version: 2022_07_17_011011) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2022_07_16_073002) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
