d7ab20ac9b160628292192130fadeaec2a5a6d85
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-07-18 07:53:54 +0000

    [Add]ログインページの編集まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,14 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all(book_params)
   end
 
   def show
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title,:body,:image)
   end
 end
