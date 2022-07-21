d18f22bafbdaf4745b6a356ddd34c82767f03f25
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-07-21 10:30:45 +0000

    userのアップデート機能まで

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to:"homes#top"
   devise_for :users
   get '/home/about' => 'homes#about',as:'about'
-  resources :books,only:[:new,:index,:show,:create]
-  resources :users,only:[:show,:edit]
+  post '/users/:id/edit' => 'users#update'
+  resources :books,only:[:new,:index,:show,:create,:edit,:destroy,:update]
+  resources :users,only:[:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
